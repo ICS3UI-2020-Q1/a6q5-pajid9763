c539baa28d33f48a38e185318c53291b3e9b2efe
pajid9763 <pajid9763@wrdsb.ca>
2020-10-08 15:59:24 +0000

    finished program that swaps values for ascending order

--- Main.java
+++ Main.java
@@ -1,15 +1,36 @@
+import java.util.Scanner;
+
 /**
- *
- * @author 
+ * program thats swaps values for ascending order
+ * 
+ * @author Darian
  */
 public class Main {
 
   /**
    * The method that is executed when you hit the run button.
+   * 
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    Scanner input = new Scanner(System.in);
+
+    double values[] = new double[2];
+
+    // ask the user for two values
+    System.out.println("Please enter the 2 values for the array");
+    for (int i = 0; i < values.length; i++) {
+      values[i] = input.nextDouble();
+    }
+
+    if (values[0] >= values[1]) {
+      double temp = values[0];
+      values[0] = values[1];
+      values[1] = temp;
+      System.out.println("Your numbers in ascending order are " + values[0] + " and " + values[1]);
+
+    } else if (values[0] <= values[1]) {
+      System.out.println("Your numbers in ascending order are " + values[0] + " and " + values[1]);
+    }
   }
 }
